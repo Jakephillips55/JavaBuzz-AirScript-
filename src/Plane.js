1b004609d20f3431fda23e764f392c7f9376cd90
Jakephillips55 <Jakep629@gmail.com>
2020-01-21 13:35:56 +0000

    update

--- src/Plane.js
+++ src/Plane.js
@@ -4,3 +4,10 @@ function Plane(){}
 Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
 };
+
+ // Functions are one of the fundamental building blocks
+ // in JavaScript. A function is a JavaScript
+ // procedure—a set of statements that performs
+ //  a task or calculates a value. To use a function,
+ // you must define it somewhere in the scope from which
+ // you wish to call it.
